2683346938b0379d0844213a9d40bb2be4059969
Hailan Wang <hailan6257@email.com>
2022-01-06 13:02:11 -0500

    Update students.js and teachers.js to use parameterized queries

---
+++ bootcamp_app/students.js
@@ -0,0 +1,24 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'hailanwang',
+  password: '',
+  host: 'localhost',
+  port: '5431',
+  database: 'bootcampx'
+});
+const text = `
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT ${process.argv[3] || 5};
+`;
+const values = [`%${process.argv[2]}%`];
+
+pool.query(text, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    });
+  }).catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,16 +7,18 @@ const pool = new Pool({
   port: '5431',
   database: 'bootcampx'
 });
-
-pool.query(`
+const text = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort 
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`;
+const values = [`%${process.argv[2]}%`];
+
+pool.query(text, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.cohort}: ${user.teacher} `);
